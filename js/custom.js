ec70cbff25376a0a642d04ba4d6e1ad021aa6eb2
abiflow <38135250+abiflow@users.noreply.github.com>
2020-12-17 20:32:34 +0000

    CV ON THE WEB

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+
+
+(function ($) {
+    
+    // About section scroll
+    $(".overlay-detail a").on('click', function(event) {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900, function(){
+            window.location.hash = hash;
+        });
+    });
+       
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar-default").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+})(jQuery);
